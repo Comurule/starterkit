d2a5265de56747a6ac5e72fbb2c59ff6a0da1fbe
Ubuntu <ubuntu@ip-172-31-89-216.ec2.internal>
2020-06-25 21:56:59 +0000

    User Module Development

---
+++ routes/user.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var tools = require('./../modules/tools');
+
+router.get('/', function(req, res, next) {
+    var viewData = {
+        title: 'Main user page',
+        userContents: 'This is main user page - it should use different layout',
+        startDate: tools.convertMillisecondsToStringDate(req.session.startDate),
+        endDate: tools.convertMillisecondsToStringDate(req.session.lastRequestDate),
+        user: req.user,
+    }
+    res.render('pages/userMain', viewData);
+});
+
+module.exports = router;
